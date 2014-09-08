d8bb4a9c6e7e847effcba3257da8deb7c89cc218
camshaft <bytheway.cameron@gmail.com>
2014-09-07 21:41:02 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,150 @@
+// https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/HTML5/HTML5_element_list
+module.exports = [
+  // shortest first
+  'a',
+  'p',
+  'q',
+  'i',
+  'b',
+  'u',
+  's',
+
+  // commonly used
+  'span',
+  'section',
+  'li',
+
+  // root
+  'html',
+
+  // metadata
+  'head',
+  'title',
+  'base',
+  'link',
+  'meta',
+  'style',
+
+  // scripting
+  'script',
+  'noscript',
+  'template',
+
+  // sections
+  'body',
+  'section',
+  'nav',
+  'article',
+  'aside',
+  'h1',
+  'h2',
+  'h3',
+  'h4',
+  'h5',
+  'h6',
+  'header',
+  'footer',
+  'address',
+  'main',
+
+  // grouping
+  // - p
+  'hr',
+  'pre',
+  'blockquote',
+  'ol',
+  // - ul
+  'li',
+  'dl',
+  'dt',
+  'dd',
+  'figure',
+  'figcaption',
+  // - div
+
+  // text-level semantics
+  // - a
+  'em',
+  'strong',
+  'small',
+  // - s
+  'cite',
+  // - q
+  'dfn',
+  'abbr',
+  'data',
+  'time',
+  'code',
+  'var',
+  'samp',
+  'kbd',
+  'sub',
+  'sup',
+  // - i
+  // - b
+  // - u
+  'mark',
+  'ruby',
+  'rt',
+  'rp',
+  'bdi',
+  'bdo',
+  // - span
+  'br',
+  'wbr',
+
+  // edits
+  'ins',
+  'del',
+
+  // embedded
+  'img',
+  'iframe',
+  'embed',
+  'object',
+  'param',
+  'video',
+  'audio',
+  'source',
+  'track',
+  'canvas',
+  'map',
+  'area',
+  'svg',
+  'math',
+
+  // tabular data
+  'table',
+  'caption',
+  'colgroup',
+  'col',
+  'tbody',
+  'thead',
+  'tfoot',
+  'tr',
+  'td',
+  'th',
+
+  // forms
+  'form',
+  'fieldset',
+  'legend',
+  'label',
+  'input',
+  'button',
+  'select',
+  'datalist',
+  'optgroup',
+  'option',
+  'textarea',
+  'keygen',
+  'output',
+  'progress',
+  'meter',
+
+  // interactive elements
+  'details',
+  'summary',
+  'menuitem',
+  'menu'
+];
